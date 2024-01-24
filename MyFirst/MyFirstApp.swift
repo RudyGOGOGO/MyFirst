e60bced98a7328f923c23642a0010d0a0d1e3d10
Wei Zhang <rudyzhang073@gmail.com>
2024-01-24 01:21:12 -0500

    Initial Commit

---
+++ MyFirst/MyFirstApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFirstApp.swift
+//  MyFirst
+//
+//  Created by Wei Zhang on 1/24/24.
+//
+
+import SwiftUI
+
+@main
+struct MyFirstApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
