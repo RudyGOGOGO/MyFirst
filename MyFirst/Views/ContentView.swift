9a9b7e25c815083b2076ae0e926eaafd0f323af9
Wei Zhang <rudyzhang073@gmail.com>
2024-01-31 23:09:39 -0500

    made changes per PR review feedback

--- MyFirst/Views/ContentView.swift
+++ MyFirst/Views/ContentView.swift
@@ -7,13 +7,11 @@ struct ContentView: View {
   @State private var blueSliderValue: Double = 75.0
   private let colorMaxConstant: Double = 255
   private let colorMinConstant: Double = 0
-  private let topFont: Font = /*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/
-  private let sliderFont: Font = .title3
   
   var body: some View {
     VStack {
       Text("Color Picker")
-        .font(topFont)
+        .font(.title)
       //TODO: how to set the relative width and height for the shape to make it automatically fill the screen when the screen gets oriented
       RoundedRectangle(cornerRadius: 0)
         .fill(shapeColor)
@@ -21,24 +19,24 @@ struct ContentView: View {
       
       //TODO: need to define a model to encapsulate the following components - Text title && slider && slider value
       Text("Red")
-        .font(sliderFont)
+        .font(.title3)
       HStack{
-        Slider(value: $redSliderValue, in:colorMinConstant...colorMaxConstant)
-        Text("\(getColorInInt(colorInDouble: redSliderValue))")
+        Slider(value: $redSliderValue, in:colorMinConstant ... colorMaxConstant)
+        Text(colorValueAsString(colorValue: redSliderValue))
       }
       
       Text("Green")
-        .font(sliderFont)
+        .font(.title3)
       HStack{
-        Slider(value: $greenSliderValue, in:colorMinConstant...colorMaxConstant)
-        Text("\(getColorInInt(colorInDouble: greenSliderValue))")
+        Slider(value: $greenSliderValue, in:colorMinConstant ... colorMaxConstant)
+        Text(colorValueAsString(colorValue: greenSliderValue))
       }
       
       Text("Blue")
-        .font(sliderFont)
+        .font(.title3)
       HStack{
-        Slider(value: $blueSliderValue, in:colorMinConstant...colorMaxConstant)
-        Text("\(getColorInInt(colorInDouble: blueSliderValue))")
+        Slider(value: $blueSliderValue, in:colorMinConstant ... colorMaxConstant)
+        Text(colorValueAsString(colorValue: blueSliderValue))
       }
       
       Button("Set Color", action: changeColor)
@@ -52,9 +50,10 @@ struct ContentView: View {
                             blue: blueSliderValue/colorMaxConstant)
   }
   
-  func getColorInInt(colorInDouble: Double) -> Int {
-    return Int(colorInDouble.rounded())
+  func colorValueAsString(colorValue: Double) -> String {
+    "\(Int(colorValue.rounded()))"
   }
+
 }
 
 #Preview {
